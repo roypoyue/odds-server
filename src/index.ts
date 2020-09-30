a5c8f5b20ce44ed72c0e91b7a53f0c7f8de0ba26
Roy Liu <roy.liu@poyue.com.tw>
2020-09-30 15:52:08 +0800

    fix cors

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,19 @@ import { StatusCodes } from "http-status-codes";
 import HttpException from "./exceptions/HttpException";
 import errorMiddleware from "./middlewares/error.middleware";
 import * as computerController from "./controllers/computer";
-import bodyParser from "body-parser";
+// import bodyParser from "body-parser";
+import cors from "cors";
 
 const app: Express = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(cors());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded());
 
-app.get("/", (req: Request, res: Response) => {
-  console.log(req);
+app.get("/", (_req: Request, res: Response) => {
   res.send("hello world");
 });
-app.get("/Reflex", computerController.getReflexByOdds);
+app.get("/getReflex", computerController.getReflexByOdds);
 
 app.use((_req: Request, _res: Response, next: NextFunction) => {
   const error: HttpException = new HttpException(
